a3a0b12fdbe0de4139d1a3bf40d2723e063f0476
Umair Vatao <umair.vatao@2359media.com>
2012-02-23 11:11:50 +0800

    Setting position of image to center while zooming out

--- src/com/svg/test/MyImageView.java
+++ src/com/svg/test/MyImageView.java
@@ -41,8 +41,13 @@ public class MyImageView extends ImageView implements OnTouchListener{
 	private Matrix matrix;
 	private Matrix savedMatrix;
 	
-	int height, width;
+	int pictureHeight, pictureWidth;
+	int viewerHeight, viewerWidth;
+	
+	float initialPicX, initialPicY;
     Picture picture;
+    
+    public static final int KEY_TRANS_X = 0, KEY_TRANS_Y = 1;
 	
 	public MyImageView(Context context) {
 		super(context);
@@ -72,16 +77,24 @@ public class MyImageView extends ImageView implements OnTouchListener{
 		
 		SVG svg = SVGParser.getSVGFromResource(getResources(), R.raw.example_map);
 	    picture = svg.getPicture();
-	    width = picture.getWidth();
-	    height = picture.getHeight();
+	    pictureWidth = picture.getWidth();
+	    pictureHeight = picture.getHeight();
+	    
+	    // for wrap_content viewer and picture size will be same. need to implement seperate logic for other cases.
+	    viewerHeight = pictureHeight;
+	    viewerWidth = pictureWidth;
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		
-		if(matrix == null)
+		if(matrix == null) {
 			matrix = canvas.getMatrix();
+			matrix.getValues(f);
+			initialPicX = f[Matrix.MTRANS_X];
+			initialPicY = f[Matrix.MTRANS_Y];
+		}
 		
 	    canvas.setMatrix(matrix);
 	    canvas.drawPicture(picture);
@@ -94,7 +107,7 @@ public class MyImageView extends ImageView implements OnTouchListener{
 	@Override
 	protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec){
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		setMeasuredDimension(width, height);
+		setMeasuredDimension(pictureWidth, pictureHeight);
 	}
 	
 	@Override
@@ -114,7 +127,6 @@ public class MyImageView extends ImageView implements OnTouchListener{
 			     midX = ( event.getX(0) + event.getX(1)) /2;
 			     midY = ( event.getY(0) + event.getY(1)) /2;
 				 mode = ZOOM;
-				 
 			 }
 			 break;
 		 case MotionEvent.ACTION_UP:
@@ -124,15 +136,40 @@ public class MyImageView extends ImageView implements OnTouchListener{
 			case MotionEvent.ACTION_MOVE:
 				if (mode == DRAG) {
 					matrix.set(savedMatrix);
-					matrix.postTranslate(event.getX() - startX,
-							event.getY() - startY);
+					
+					
+					float[] desiredTranslations = new float[2];
+					
+					
+					desiredTranslations[KEY_TRANS_X] = event.getX() - startX;
+					desiredTranslations[KEY_TRANS_Y] = event.getY() - startY;
+					
+					
+					Log.d("2359", "Desired Translation x: " + desiredTranslations[KEY_TRANS_X] +" y:"+ desiredTranslations[KEY_TRANS_Y]);
+					
+					
+					calculateRestrictedTranslations(desiredTranslations);
+					
+					Log.d("2359", "Translating x: " + desiredTranslations[KEY_TRANS_X] +" y:"+ desiredTranslations[KEY_TRANS_Y]);
+					matrix.postTranslate(desiredTranslations[KEY_TRANS_X], desiredTranslations[KEY_TRANS_Y]);
+					
+					
+					
 				}
 				else if (mode == ZOOM) {
 					float newDist = spacing(event);
 					if (newDist > 10f) {
 						matrix.set(savedMatrix);
 						scale = newDist / oldDist;
+						// restrict scale
+						scale = calculateRestrictedScale(scale, 1.0f, 3.0f);
+						
+						Log.d("2359", "Changed scale:"+scale);
 						matrix.postScale(scale, scale, midX, midY);
+						
+						adjustXYAfterScaling();
+						
+						
 						matrix.getValues(f);
 						totalScale = f[Matrix.MSCALE_X];
 					}
@@ -142,6 +179,95 @@ public class MyImageView extends ImageView implements OnTouchListener{
 		 }
 
 		 return true;
+	}
+	
+	private void adjustXYAfterScaling(){
+		float[] mValues = new float[9];
+		matrix.getValues(mValues);
+		
+		float currentX = mValues[Matrix.MTRANS_X];
+		float currentY = mValues[Matrix.MTRANS_Y];
+		float adjustX = 0, adjustY = 0;
+		
+		
+		float currentWidth = (int) (pictureWidth * mValues[Matrix.MSCALE_X]);
+		float currentHeight = (int) (pictureHeight * mValues[Matrix.MSCALE_Y]); 
+		
+		float maxX = initialPicX; // we supporting min zoom 1, increasing from this X or Y value means we are creating white space on left or top.
+		float maxY = initialPicY; // min zoom is 1
+		
+		float minX = (initialPicX + viewerWidth - currentWidth);
+		float minY = (initialPicY + viewerHeight - currentHeight);
+		
+		
+		if(currentX < minX) {
+			adjustX =  (minX - currentX);
+		}
+		if(currentY < minY){
+			adjustY = (minY - currentY);
+		}
+		if(currentX > maxX) {
+			adjustX = (maxX - currentX);
+		}
+		if(currentY > maxY) {
+			adjustY = (maxY - currentY);
+		}
+		
+		
+		matrix.postTranslate(adjustX, adjustY);
+	}
+	
+	private void calculateRestrictedTranslations(float[] desiredTranslations){
+		float[] mValues = new float[9];
+		matrix.getValues(mValues);
+		float currentWidth = (int) (pictureWidth * mValues[Matrix.MSCALE_X]);
+		float currentHeight = (int) (pictureHeight * mValues[Matrix.MSCALE_Y]); 
+		
+		
+		
+		float maxX = initialPicX; // we supporting min zoom 1, increasing from this X or Y value means we are creating white space on left or top.
+		float maxY = initialPicY; // min zoom is 1
+		
+		// if viewerWidth is 100, and image's currentWidth (after scale etc) is 200.. then we need to set the X position to -100 to see the ending portion of image i.e. from 101 to 200px part.
+		// initialPicX and initialPicY variable is introduced coz, initial X and Y may not be zero in all cases. for understanding of concept ignore them.
+		float minX = (initialPicX + viewerWidth - currentWidth);
+		float minY = (initialPicY + viewerHeight - currentHeight);
+		
+		// getting projected values, will check them if they comply with our boundries.. otherwise will adjust user's desired translation.
+		float projectedX = mValues[Matrix.MTRANS_X] + desiredTranslations[KEY_TRANS_X];
+		float projectedY = mValues[Matrix.MTRANS_Y] + desiredTranslations[KEY_TRANS_Y];
+		
+		if(projectedX < minX) {
+			desiredTranslations[KEY_TRANS_X] = desiredTranslations[KEY_TRANS_X] + (minX - projectedX);
+		}
+		if(projectedY < minY){
+			desiredTranslations[KEY_TRANS_Y] = desiredTranslations[KEY_TRANS_Y] + (minY - projectedY);
+		}
+		if(projectedX > maxX) {
+			desiredTranslations[KEY_TRANS_X] = desiredTranslations[KEY_TRANS_X] - (projectedX - maxX);
+		}
+		if(projectedY > maxY) {
+			desiredTranslations[KEY_TRANS_Y] = desiredTranslations[KEY_TRANS_Y] - (projectedY - maxY);
+		}
+	}
+	
+	private float calculateRestrictedScale(float desiredScale, float minZoom, float maxZoom){
+		// restricting scale
+		float[] tempMatrixVals = new float[9];
+		float currentScale, predictedZoom;
+		matrix.getValues(tempMatrixVals);
+		currentScale = tempMatrixVals[Matrix.MSCALE_X];
+		
+		predictedZoom = desiredScale * currentScale;
+		
+		if(predictedZoom > maxZoom) {
+			desiredScale = maxZoom / currentScale;
+		}
+		if(predictedZoom < minZoom){
+			desiredScale =  minZoom / currentScale;
+		}
+		
+		return desiredScale;
 	}
 	
 	private float spacing(MotionEvent event) {
